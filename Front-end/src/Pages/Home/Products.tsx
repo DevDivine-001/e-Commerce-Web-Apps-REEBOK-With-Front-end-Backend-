562f621b4cb6005f9bb3e1bf08dcdc6772c2167e
Oscdivin <divineuwandu84@Gmail.com>
2024-05-07 22:32:46 +0100

    SeeProducts pagesis done now

--- Front-end/src/Pages/Home/Products.tsx
+++ Front-end/src/Pages/Home/Products.tsx
@@ -1,82 +1,102 @@
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const products = [
-  {
-    id:1,
-    img:"",
-    text:"Best Deals",
-  },
-  {
-    id:2,
-    img:"",
-    text:"Refrigerators",
-  },
-  {
-    id:3,
-    img:"",
-    text:"Televisions",
-  },
-  {
-    id:4,
-    img:"",
-    text:"Special Offers",
-  },
-  {
-    id:5,
-    img:"",
-    text:"Fashion Deals",
-  },
-  {
-    id:6,
-    img:"",
-    text:"Generators",
-  },
-  {
-    id:7,
-    img:"",
-    text:"Phones & Tablets",
-  },
-  {
-    id:8,
-    img:"",
-    text:"Computing Deals",
-  },
-  {
-    id:9,
-    img:"",
-    text:"Grocery Deals",
-  },
-  {
-    id:10,
-    img:"",
-    text:"Mobile Accessories",
-  },
-  {
-    id:11,
-    img:"",
-    text:"Home Essentials",
-  },
-  {
-    id:12,
-    img:"",
-    text:"Call to Order",
-  },
-]
+// const products = [
+//   {
+//     id:1,
+//     img:"",
+//     text:"Best Deals",
+//   },
+//   {
+//     id:2,
+//     img:"",
+//     text:"Refrigerators",
+//   },
+//   {
+//     id:3,
+//     img:"",
+//     text:"Televisions",
+//   },
+//   {
+//     id:4,
+//     img:"",
+//     text:"Special Offers",
+//   },
+//   {
+//     id:5,
+//     img:"",
+//     text:"Fashion Deals",
+//   },
+//   {
+//     id:6,
+//     img:"",
+//     text:"Generators",
+//   },
+//   {
+//     id:7,
+//     img:"",
+//     text:"Phones & Tablets",
+//   },
+//   {
+//     id:8,
+//     img:"",
+//     text:"Computing Deals",
+//   },
+//   {
+//     id:9,
+//     img:"",
+//     text:"Grocery Deals",
+//   },
+//   {
+//     id:10,
+//     img:"",
+//     text:"Mobile Accessories",
+//   },
+//   {
+//     id:11,
+//     img:"",
+//     text:"Home Essentials",
+//   },
+//   {
+//     id:12,
+//     img:"",
+//     text:"Call to Order",
+//   },
+// ]
 
 const Products = () => {
   return (
     <div className="flex justify-center items-center w-[100%] h-[87vh] bg-[]">
-      <section className="w-[93%] h-[87vh] bg-[#fff] rounded-[7px] flex  shadow-md flex-wrap justify-center  items-center">
+      <section className="w-[93%] h-[87vh] bg-[#fff] rounded-[7px] flex  shadow-md flex-wrap justify-center  items-center p-2">
         <div className="w-[100%] h-[85vh]  flex-wrap justify-between flex  items-center max-sm:w-full">
 
-             {
+          <div className="flex 
+          p-2 bg-[red] flex-col w-[16%] gap-1 max-sm:w-28">
+            <img src="" alt="" className="w-[98%] h-[29vh] flex items-center justify-center max-sm:h-[100px]"/>
+            <button>Best School play</button>
+
+          </div>
+          <div className="flex 
+          p-2 bg-[red] flex-col w-[16%] gap-1 max-sm:w-28">
+            <img src="" alt="" className="w-[98%] h-[29vh] flex items-center justify-center max-sm:h-[100px]"/>
+            <button>Best</button>
+
+          </div>
+          {/* <div className="flex justify-center items-center
+          p-2 bg-[red] flex-col w-[16%] gap-1 max-sm:w-28 max-sm:h-24">
+            <img src="" alt="" className="w-[98%] h-[2vh] flex items-center  justify-center "/>
+            <button>Best Deals</button>
+
+          </div> */}
+
+
+             {/* {
               products.map((data) => (
                  <Link to="/see-products" className="flex justify-center items-center flex-col bg-[red] w-[16%]  max-sm:w-[32%] max-sm:h-[20vh] rounded-[7px] max-sm:flex" key={data.id}>
                 <img src="" alt="" className="w-[93%] h-[40vh] bg-[blue] object-cover flex justify-center items-center border-none rounded-[7px] cursor-pointer max-sm:w-full"/>
       <Link to="/see-products">          <button className="cursor-pointer">{data.text}</button></Link></Link >
               ))
-             }
+             } */}
             
         
        
--- Front-end/src/Pages/Home/SeeProducts.tsx
+++ Front-end/src/Pages/Home/SeeProducts.tsx
@@ -1,5 +1,86 @@
+// import img2 from "../../assets/712x384copy2-treasurehunt.jpeg"
 const SeeProducts = () => {
-  return <div>SeeProducts</div>;
+  const Products = [
+    {
+      id:1,
+      img:"",
+      productsName:"Mobile Accessories"
+    },
+    {
+      id:2,
+      img:"",
+      productsName:"Home Essentials"
+    },
+    {
+      id:3,
+      img:"",
+      productsName:"Best Deals"
+    },
+      {
+    id:4,
+    img:"",
+   productsName:"Phones & Tablets",
+  },
+    {
+    id:5,
+    img:"",
+   productsName:"Computing Deals",
+  },
+    {
+    id:6,
+    img:"",
+   productsName:"Grocery Deals",
+  },
+    {
+    id:7,
+    img:"",
+   productsName:"Grocery Deals",
+  },
+    {
+    id:8,
+    img:"",
+   productsName:"Grocery Deals",
+  },
+    {
+    id:9,
+    img:"",
+   productsName:"Grocery Deals",
+  },
+    {
+    id:10,
+    img:"",
+   productsName:"Grocery Deals",
+  },
+    {
+    id:11,
+    img:"",
+   productsName:"Grocery Deals",
+  },
+  {
+  id:12,
+  img:"",
+  productsName:"Grocery Deals",
+  },
+  
+]
+  return<div className="w-[100%] h-auto flex justify-center items-center bg-[]">
+     <div className="w-[93%] py-[1%] flex justify-center items-center max-sm:py-[3%] max-lg:py-[3%]">
+       <div className="grid grid-cols-3 gap-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 p-2 max-sm:w-full w-[100%] bg-[] lg:grid-cols-4">
+ {Products.map((data)=> (
+       <div className="video" key={data .id}>
+      
+<img src={data.img} className="w-[100%] py-[60%] bg-[red] flex items-center justify-center rounded-md object-cover max-sm:py-[55%]" alt="" />
+       <div className="flex items-center justify-center text-wrap"> <button className="text-sm sm:text-sm text-[px] text-wrap text-center max-lg:text-bold">{data.productsName}</button></div>
+       </div>
+  ))
+ }
+
+  </div>
+     </div>
+  </div>
+
+  
+  ;
 };
 
 export default SeeProducts;
