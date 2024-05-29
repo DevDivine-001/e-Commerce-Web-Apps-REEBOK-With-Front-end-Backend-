33d30dab3bae356d26df98c9a82e471681a3cbb5
Oscdivin <divineuwandu84@Gmail.com>
2024-05-29 15:44:01 +0100

    SeeProducts all is done

--- Front-end/src/Pages/Home/SeeProducts.tsx
+++ Front-end/src/Pages/Home/SeeProducts.tsx
@@ -3,32 +3,32 @@ const SeeProducts = () => {
   const Products = [
     {
       id:1,
-      img:"",
+      img:"https://images.pexels.com/photos/4158/apple-iphone-smartphone-desk.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       productsName:"Mobile Accessories"
     },
     {
       id:2,
-      img:"",
+      img:"https://i.im.ge/2024/05/08/ZVFqaM.Made-in-naija-removebg-preview-1.png",
       productsName:"Home Essentials"
     },
     {
       id:3,
-      img:"",
+      img:"https://i.im.ge/2024/05/08/ZVFqaM.Made-in-naija-removebg-preview-1.png",
       productsName:"Best Deals"
     },
       {
     id:4,
-    img:"",
+    img:"https://i.im.ge/2024/05/08/ZVFqaM.Made-in-naija-removebg-preview-1.png",
    productsName:"Phones & Tablets",
   },
     {
     id:5,
-    img:"",
+    img:"https://i.im.ge/2024/05/08/ZVFqaM.Made-in-naija-removebg-preview-1.png",
    productsName:"Computing Deals",
   },
     {
     id:6,
-    img:"",
+    img:"https://i.im.ge/2024/05/08/ZVFqaM.Made-in-naija-removebg-preview-1.png",
    productsName:"Grocery Deals",
   },
     {
@@ -1564,16 +1564,19 @@ const SeeProducts = () => {
   
   className="w-[100%] h-auto flex justify-center items-center bg-[]">
      <div className="w-[93%] py-[1%] flex justify-center items-center max-sm:py-[3%] max-lg:py-[3%]">
-       <div className="grid grid-cols-1 gap-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 p-2 max-sm:w-full w-[100%] bg-[] lg:grid-cols-4">
+       <div className="grid grid-cols-3 gap-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 p-2 max-sm:w-full w-[100%] bg-[] lg:grid-cols-4">
  {Products.map((data)=> (
-   <div className="TopProducts" key={data .id}
+   <div className="" key={data .id}
    data-aos="fade-right"
                 data-aos-delay={data.aosDelay}
    >
-
+    <div className="SeeProducts">
       
-<img src={data.img} className="w-[100%] py-[60%] bg-[red] flex items-center justify-center rounded-md object-cover max-sm:py-[55%]" alt="" />
+<div className="flex items-center justify-center object-cover bg-[blue] w-[100%] h-[30vh] max-sm:h-[85px] max-md:h-[90px] max-lg:h-[170px] rounded-md">
+  <img src={data.img} className="w-full h-full bg-[] flex items-center justify-center rounded-md  " alt="" />
+</div>
        <div className="flex items-center justify-center text-wrap"> <button className="text-sm sm:text-sm text-[px] text-wrap text-center max-lg:text-bold">{data.productsName}</button></div>
+       </div>
        </div>
   ))
  }
